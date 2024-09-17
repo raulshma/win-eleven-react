85fb728ce6713c27a04736e886a61d862b56dc7a
raulshma <raulshma@gmail.com>
2024-09-17 20:01:24 +0530

    Taskbar right

--- src/components/common/TaskbarRight.tsx
+++ src/components/common/TaskbarRight.tsx
@@ -1,9 +1,29 @@
+import { ChevronUpIcon } from "lucide-react";
+
 function TaskbarRight() {
   return (
     <div className="flex flex-row-reverse items-center px-3 py-1">
-      <div className="flex items-center py-[2px] px-2">
-        
+      <div className="flex items-center py-[2px] px-2 gap-4">
+        <TrayIcons />
+        <LanguageSelector />
       </div>
+    </div>
+  );
+}
+
+function TrayIcons() {
+  return (
+    <>
+      <ChevronUpIcon className="h-5 w-5"/>
+    </>
+  );
+}
+
+function LanguageSelector() {
+  return (
+    <div className="text-xs flex flex-col text-center">
+      <p>ENG</p>
+      <p>IN</p>
     </div>
   );
 }
